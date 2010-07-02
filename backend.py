34a1a104f9a7a93d6c28048113295027e6a25d91
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-02 18:09:12 +0000

    Initial upload; basic AppEngine structure
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@2 c84bbe80-f878-e576-0e26-96495b094a5b

---
+++ backend.py
@@ -0,0 +1,3 @@
+
+def search():
+	return "It's me, the backend! Let's do this!\n"
---
+++ main.py
@@ -0,0 +1,17 @@
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+import backend
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write("I'm the frontend. Let me go get the backend for you...<br />")
+	answer = backend.search()
+	self.response.out.write(answer)
+
+def main():
+    application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
+    util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+    main()
