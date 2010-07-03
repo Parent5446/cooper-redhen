2ee9df43cbeae59f0675bbdd7d56fb1a2f2daa08
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-03 17:19:07 +0000

    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@6 c84bbe80-f878-e576-0e26-96495b094a5b

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 
+import os
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp.util import users
 import backend
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write("I'm the frontend. Let me go get the backend for you...<br />")
-	answer = backend.search()
-	self.response.out.write(answer)
+	name = users.get_current_user()
+	greeting = 'Hello'
+        template_values = {
+            'greeting': greeting,
+            'userName': name
+            }
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
