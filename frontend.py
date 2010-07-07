d2e3c6ded3d6104bafebc11d5160d384b45c09b0
elnerdo <elnerdo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 15:57:17 +0000

    Fixing the indentation error
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@32 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -4,7 +4,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp import util
 from google.appengine.api import users
-#import backend
+import backend
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
@@ -13,12 +13,9 @@ class MainHandler(webapp.RequestHandler):
 		#testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
 		if user is not None: name = user.nickname()
 		greeting = 'elcome!'
-			template_values = {
-				'greeting': greeting,
-				'userName': name
-				}
-			path = os.path.join(os.path.dirname(__file__), 'index.html')
-			self.response.out.write(template.render(path, template_values))
+		template_values = {'greeting': greeting, 'userName': name }
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
 
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
