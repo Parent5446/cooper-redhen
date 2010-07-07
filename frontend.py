2b07f69e2bbdf0d48d5013c227a251403ee4a57a
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 19:13:15 +0000

    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@71 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -1,16 +1,32 @@
-﻿from google.appengine.api import users
+﻿import cgi
+
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        if user:
-            self.response.out.write('<html style=text-align:center><h1>Hello, '+user.nickname()+'</h1></html>')
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+        self.response.out.write("""
+          <html>
+            <body>
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
 
-application = webapp.WSGIApplication( [('/', MainPage)], debug=True)
+
+class Guestbook(webapp.RequestHandler):
+    def post(self):
+        self.response.out.write('<html><body>You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                      ('/sign', Guestbook)],
+                                     debug=True)
 
 def main():
     run_wsgi_app(application)
