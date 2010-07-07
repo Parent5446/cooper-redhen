675d7fb2c8d6349bdfbf975d6630f261438655e9
elnerdo <elnerdo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 17:43:17 +0000

    Fixed the crazy arrows.
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@43 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -8,22 +8,17 @@ from google.appengine.api import users
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-<<<<<<< .mine
 	user = users.get_current_user()
 	name = 'Red Hen User'
 	if user is not None: name = user.nickname()
-<<<<<<< .mine
 	greeting = 'How u doin? :)'
-=======
 	greeting = 'How u doin my friend?'
->>>>>>> .r19
         template_values = {
             'greeting': greeting,
             'userName': name
             }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
-=======
 		user = users.get_current_user()
 		name = 'Red Hen User'
 		testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
@@ -32,8 +27,6 @@ class MainHandler(webapp.RequestHandler):
 		template_values = {'greeting': greeting, 'userName': name, 'testdata':testdata }
 		path = os.path.join(os.path.dirname(__file__), 'index.html')
 		self.response.out.write(template.render(path, template_values))
->>>>>>> .r22
-
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
     util.run_wsgi_app(application)
