b32831e92dd489e3c77420b1bce42660f7685d50
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 19:05:13 +0000

    Made frontend not plain text
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@69 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -7,7 +7,6 @@ class MainPage(webapp.RequestHandler):
         user = users.get_current_user()
 
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
             self.response.out.write('Hello, ' + user.nickname())
         else:
             self.redirect(users.create_login_url(self.request.uri))
