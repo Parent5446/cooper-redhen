8e85e62496ce3a3387449af640439c8c27603e27
elnerdo <elnerdo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 15:52:42 +0000

    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@28 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -10,13 +10,12 @@ class MainHandler(webapp.RequestHandler):
     def get(self):
 		user = users.get_current_user()
 		name = 'Red Hen User'
-		testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
+		#testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
 		if user is not None: name = user.nickname()
 		greeting = 'elcome!'
 			template_values = {
 				'greeting': greeting,
-				'userName': name,
-				'testdata':testdata
+				'userName': name
 				}
 			path = os.path.join(os.path.dirname(__file__), 'index.html')
 			self.response.out.write(template.render(path, template_values))
