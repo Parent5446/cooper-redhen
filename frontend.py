40519130d74577ac30bb8b3ba5d83a719c017baa
elnerdo <elnerdo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 17:52:56 +0000

    Added test folders for frontend project and worked on fixes for frontend.py
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@48 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -11,22 +11,12 @@ class MainHandler(webapp.RequestHandler):
 	user = users.get_current_user()
 	name = 'Red Hen User'
 	if user is not None: name = user.nickname()
-	greeting = 'How u doin? :)'
 	greeting = 'How u doin my friend?'
-        template_values = {
-            'greeting': greeting,
-            'userName': name
-            }
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
-		user = users.get_current_user()
-		name = 'Red Hen User'
-		testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
-		if user is not None: name = user.nickname()
-		greeting = 'elcome!'
-		template_values = {'greeting': greeting, 'userName': name, 'testdata':testdata }
-		path = os.path.join(os.path.dirname(__file__), 'index.html')
-		self.response.out.write(template.render(path, template_values))
+	template_values = { 'greeting': greeting, 'userName': name }
+	
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
+	
 def main():
     application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
     util.run_wsgi_app(application)
