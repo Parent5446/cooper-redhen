d55f2d0f2679bd23efba69639f30d3da26530f68
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 18:23:55 +0000

    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@60 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -4,6 +4,27 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 class MainPage(webapp.RequestHandler):
     def get(self):
+	user = users.get_current_user()
+	name = 'Red Hen User'
+	if user is not None: name = user.nickname()
+	greeting = 'How u doin my friend?'
+    template_values = {'greeting': greeting,'userName': name}
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+		user = users.get_current_user()
+		name = 'Red Hen User'
+		testdata = '<h2> This is a test <i> is this good </i> </h2> <br />poooop'
+		if user is not None: name = user.nickname()
+		greeting = 'elcome!'
+		template_values = {'greeting': greeting, 'userName': name, 'testdata':testdata }
+		self.redirect('http://www.google.com');
+		path = os.path.join(os.path.dirname(__file__), 'index.html')
+		self.response.out.write(template.render(path, template_values))
+
+	template_values = { 'greeting': greeting, 'userName': name }
+	
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, template_values))
         user = users.get_current_user()
 
         if user:
@@ -13,6 +34,7 @@ class MainPage(webapp.RequestHandler):
             self.redirect(users.create_login_url('/'))
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+
 
 def main():
     run_wsgi_app(application)
