967522ac609cbc521d6712b9e7ac5aaf1f3566ce
elnerdo <elnerdo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-07 18:01:12 +0000

    Replaced frontend.py with a simple 'hello world' example.
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@51 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -6,20 +6,18 @@ from google.appengine.ext.webapp import util
 from google.appengine.api import users
 #import backend
 
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-	user = users.get_current_user()
-	name = 'Red Hen User'
-	if user is not None: name = user.nickname()
-	greeting = 'How u doin my friend?'
-	template_values = { 'greeting': greeting, 'userName': name }
-	
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
-	
-def main():
-    application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
-    util.run_wsgi_app(application)
 
-if __name__ == '__main__':
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+
+def main():
+	application = webapp.WSGIApplication(
+                                     [('/', MainPage)],
+                                     debug=True)
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
     main()
