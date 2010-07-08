cba278f05904413855bbf330257bc8566fb77787
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-08 14:59:02 +0000

    fixed import syntax error
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@78 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -3,6 +3,7 @@
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
 class MainPage(webapp.RequestHandler):
     def get(self):
