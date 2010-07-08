75c13004d801bdc5d5c74be85cbe1366bca45aae
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-08 15:16:14 +0000

    figuring out how the db stuff works
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@86 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -5,38 +5,44 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write("""
-          <form action="/sign" enctype="multipart/form-data" method="post">
-            <div><label>Upload JDX file:</label></div>
-            <div><input type="file" name="img"/></div><br />
-			<div><label>Name of JDX file:</label></div>
-			<div><input type="text" name="content"/></div>
-            <div><input type="submit" value="Upload"></div>
-          </form>
-        </body>
-      </html>""")
-
 class Greeting(db.Model):
     author = db.UserProperty()
     content = db.StringProperty(multiline=True)
-    avatar = db.BlobProperty()
     date = db.DateTimeProperty(auto_now_add=True)
-	
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('<html><body>')
+
+        greetings = db.GqlQuery("SELECT * FROM Greeting ORDER BY date DESC LIMIT 10")
+
+        for greeting in greetings:
+            if greeting.author:
+                self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+            else:
+                self.response.out.write('An anonymous person wrote:')
+            self.response.out.write('<blockquote>%s</blockquote>' %
+                                    cgi.escape(greeting.content))
+
+        # Write the submission form and the footer of the page
+        self.response.out.write("""
+              <form action="/sign" method="post">
+                <div><textarea name="content" rows="3" cols="60"></textarea></div>
+                <div><input type="submit" value="Sign Guestbook"></div>
+              </form>
+            </body>
+          </html>""")
+
 class Guestbook(webapp.RequestHandler):
     def post(self):
         greeting = Greeting()
+
         if users.get_current_user():
             greeting.author = users.get_current_user()
-        greeting.content = self.request.get("content")
-        avatar = self.request.get("img")
-        greeting.avatar = db.Blob(avatar)
-        greeting.put()
-	self.response.out.write('<html><body>You wrote:<pre>')
-	self.response.out.write(cgi.escape(self.request.get('content')))
-	self.response.out.write('</pre></body></html>')
 
+        greeting.content = self.request.get('content')
+        greeting.put()
+        self.redirect('/')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
