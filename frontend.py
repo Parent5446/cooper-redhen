a21039843327c24a2300178f9aada17dedd772a5
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-08 16:06:06 +0000

    uploader trial one
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@102 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -1,69 +1,59 @@
-﻿import backend
-import cgi
+﻿import cgi
 import datetime
-import logging
 
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import images
 
-logging.getLogger().setLevel(logging.DEBUG)
 
-class JDXFile(db.Model):
-    fileName = db.BlobProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
+class JDX(db.Model):
+	fileName = db.BlobProperty()
+	date = db.DateTimeProperty(auto_name_add=True)
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-        query_str = "SELECT * FROM JDXFile ORDER BY date DESC LIMIT 10"
-        JDXFiles = db.GqlQuery (query_str)
-
-        '''for JDXFile in JDXFiles:
-            self.response.out.write("<div><img src='img?img_id=%s'></img>" %
-                                    JDXFile.key())
-            self.response.out.write('</div>')'''
-
-        self.response.out.write("""
-              <form action="/sign" enctype="multipart/form-data" method="post">
-                <div><label>JDX File:</label></div>
+	def get(self):
+		self.response.out.write('<html><body>')
+		query_str = "SELECT * FROM JDX ORDER BY date DESC LIMIT 10"
+		JDXs = db.GqlQuery(query_str)
+		
+		for jdx in JDXs:
+			self.response.out.write("<a>file uploaded: %s</a>" %
+									jdx.key())
+		self.response.out.write("""
+			 <form action="/uploader" enctype="multipart/form-data" method="post">
                 <div><input type="file" name="jdxFile"/></div>
                 <div><input type="submit" value="Upload"></div>
               </form>
             </body>
           </html>""")
+		  
+class JDXFile(webapp.RequestHandler):
+	def get(self):
+		jdx = db.get(self.request.get("jdxFile_id"))
+		if jdx.fileName:
+			self.response.out.write(jdx.fileName)
+		else:
+			self.response.out.write("Failed to Upload")
 
-class JDX (webapp.RequestHandler):
-    def get(self):
-        JDXFile = db.get(self.request.get("jdxFile_id"))
-        if JDXFile.fileName:
-            self.response.headers['Content-Type'] = "image/png"
-            self.response.out.write(JDXFile.fileName)
-        else:
-            self.response.out.write("Failed to Upload")
+class uploadedPage(webapp.RequestHandler):
+	def post(self):
+		jdx = JDX()
+		jdx.fileName = db.Blob(fileName)
+		jdx.put()
+		self.redirect('/')
 
-class Uploader(webapp.RequestHandler):
-    def post(self):
-        jdxFile = JDXFiles()
-        jdxFile.fileName = db.Blob(fileName)
-        jdxFile.put()
-        self.redirect('/')
-
+		
 class Test(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Testing backend...')
-        file = open('jcamp-test.jdx')
-        response = backend.search(file)
-        self.response.out.write('Done')
-
+	def get(self):
+		self.response.out.write('Testing backend...')
+		file = open('jcamp-test.jdx')
+		response = backend.search(file)
+		self.response.out.write('Done')
 
 application = webapp.WSGIApplication([
     ('/', MainPage),
-    ('/img', JDX),
-    ('/sign', Uploader),
-    ('/test', Test)
+    ('/uploader', uploadedPage)
 ], debug=True)
 
 
@@ -73,3 +63,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+		
