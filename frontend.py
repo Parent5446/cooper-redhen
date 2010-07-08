ee464ae211d23994faecaa87f0967f07afd76221
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-08 16:15:39 +0000

    Fixed frontend tabs and misspelling
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@105 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -11,49 +11,49 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 class JDX(db.Model):
-	fileName = db.BlobProperty()
-	date = db.DateTimeProperty(auto_name_add=True)
+    fileName = db.BlobProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp.RequestHandler):
-	def get(self):
-		self.response.out.write('<html><body>')
-		query_str = "SELECT * FROM JDX ORDER BY date DESC LIMIT 10"
-		JDXs = db.GqlQuery(query_str)
-		
-		for jdx in JDXs:
-			self.response.out.write("<a>file uploaded: %s</a>" %
-									jdx.key())
-		self.response.out.write("""
-			 <form action="/uploader" enctype="multipart/form-data" method="post">
+    def get(self):
+        self.response.out.write('<html><body>')
+        query_str = "SELECT * FROM JDX ORDER BY date DESC LIMIT 10"
+        JDXs = db.GqlQuery(query_str)
+        
+        for jdx in JDXs:
+            self.response.out.write("<a>file uploaded: %s</a>" %
+                                    jdx.key())
+        self.response.out.write("""
+             <form action="/uploader" enctype="multipart/form-data" method="post">
                 <div><input type="file" name="jdxFile"/></div>
                 <div><input type="submit" value="Upload"></div>
               </form>
             </body>
           </html>""")
-		  
+          
 class JDXFile(webapp.RequestHandler):
-	def get(self):
-		jdx = db.get(self.request.get("jdxFile_id"))
-		if jdx.fileName:
-			self.response.out.write(jdx.fileName)
-		else:
-			self.response.out.write("Failed to Upload")
+    def get(self):
+        jdx = db.get(self.request.get("jdxFile_id"))
+        if jdx.fileName:
+            self.response.out.write(jdx.fileName)
+        else:
+            self.response.out.write("Failed to Upload")
 
 class uploadedPage(webapp.RequestHandler):
-	def post(self):
-		jdx = JDX()
-		jdx.fileName = db.Blob(fileName)
-		jdx.put()
-		self.redirect('/')
+    def post(self):
+        jdx = JDX()
+        jdx.fileName = db.Blob(fileName)
+        jdx.put()
+        self.redirect('/')
 
-		
+        
 class Test(webapp.RequestHandler):
-	def get(self):
-		self.response.out.write('Testing backend...')
-		file = open('jcamp-test.jdx')
+    def get(self):
+        self.response.out.write('Testing backend...')
+        file = open('jcamp-test.jdx')
         backend.add(file)
-		#response = backend.search(file)
-		self.response.out.write('Done')
+        #response = backend.search(file)
+        self.response.out.write('Done')
 
 application = webapp.WSGIApplication([
     ('/', MainPage),
@@ -67,4 +67,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-		
+        
