b83cfb93e79eb6b3c6b82a0661c3aac5a58cd124
mottaqui.karim.9@gmail.com <mottaqui.karim.9@gmail.com@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-12 19:09:46 +0000

    added experimental user login functionality
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@147 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -11,6 +11,19 @@ from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import quota
+from google.appengine.api import users
+
+class MyHandler(webapp.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+        else:
+            greeting = ("<a href=\"%s\">Sign in or register</a>." %
+                        users.create_login_url("/"))
+
+        self.response.out.write("<html><body>%s</body></html>" % greeting)
 
 class Test(webapp.RequestHandler):
     def get(self):
@@ -31,7 +44,8 @@ class Test(webapp.RequestHandler):
         self.response.out.write(file_contents)
 
 application = webapp.WSGIApplication([
-	('/', Test)
+	('/', MyHandler),
+	('/test', Test)
 ], debug=True)
 
 def main():
