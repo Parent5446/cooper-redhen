ee5240b1e26bcc768f786eab19bb2c4526626ae7
tylerromeo <tylerromeo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-15 14:16:27 +0000

    New uploader.py takes a directory of files and transfers them to the server. WARNING: The application itself is not set up to receive transfers yet.
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@188 c84bbe80-f878-e576-0e26-96495b094a5b

--- uploader.py
+++ uploader.py
@@ -1,45 +1,129 @@
+from __future__ import with_statement
 import cgi
+import httplib
+import urllib
+import pickle
+import os
+import os.path
 
-from google.appengine.ext import db
-from googlre.appengine.api import users
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import images
+def main(dir):
+    if not os.path.exists(dir) or not os.path.isdir(dir):
+        raise Exception("Not a directory.")
+    files = os.listdir(dir)
+    for file_name in files:
+        with open(file_name) as file_obj:
+            transfer(file_obj)
 
-class JDXFile(db.Model):
-	fileName = db.BlobProperty()
+def transfer(file_obj):
+    # Generate a SpectrumTransfer object
+    contents = file_obj.read()
+    spectrum = SpectrumTransfer()
+    spectrum.parse_file(spectrum)
+    # Serialize into a string
+    data = pickle.dumps(spectrum)
+    
+    # Set up HTTP connection
+    host = "cooper-redhen.appspot.com"
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Accept": "text/plain"}
+    params = urllib.encode({"spectrum": data})
+    conn = HTTPConnection("cooper-redhen.appspot.com", strict=True)
+    conn.request("POST", "/upload", params, headers)
+    
+    # Process response
+    response = conn.getresponse()
+    if int(response.status) != 200:
+        raise Exception("Upload error: %s" % response.read())
+    return True
 
-class MainPage(webapp.RequestHandler):
-	def get(self): 
-		self.response.out.write('<html><body>')
-		query_str = "SELECT * FROM JDXFile DESC LIMIT 10"
-		jdxFiles = db.GqlQuery (query_str)
-			
-		for jdxFile in jdxFiles:
-			self.response.out.write("<a> File Name is: %s</a>" %
-									jdxFile.key())
-										
-		self.response.out.write("""
-			<form action="/upload" enctype="multipart/form-data" method="post">
-				<div><label>Upload File:</label></div>
-				<div><input type="file" name="file"/></div>					
-				<div><input type="submit" value="Upload!"></div>
-				</form>
-				</body></html>""")
-
-class UploadedFile(webapp.REquestHandler):
-		def post(self):
-			jdxFile = JDXFile()
-			jdxFile.fileName = db.Blob(fileName)
-			jdxFile.put()
-			self.redirect('/')
-application = webapp.WSGIApplication([
-	('/', MainPage),
-	('/sign', UploadedFile)
-], debug=True)
-
-def main():
-	run_wsgi_app(application)
-
+class SpectrumTransfer():
+    """Store a spectrum, its related data, and any algorithms necessary
+    to compare the spectrum to the DataStore."""
+    
+    ## The chemical name associated with the spectrum.
+    chemical_name = ""
+    
+    ## The chemical type of the substance the spectrum represents.
+    chemical_type = ""
+    
+    ## A list of integrated X,Y points for the spectrum's graph.
+    data = []
+    
+    ## Parse a string of JCAMP file data and extract all needed data.
+    # 
+    # Search a JCAMP file for the chemical's name, type, and spectrum data.
+    # Then integrate the X, Y data and store alGet a specific data label from the file.l variables in the object.
+    # @warning Does not handle Windows-format line breaks.
+    # 
+    # @param file_obj File descriptor for the JCAMP file.
+    def parse_file(self, file_obj):
+        """Parse a string of JCAMP file data and extract all needed data."""
+        self.contents = file_obj.read()
+        self.type = 'Infrared' # Later this will be variable
+        x = float(self.get_field('##FIRSTX=')) # The first x-value
+        deltaX = float(self.get_field('##DELTAX=')) # The Space between adjacent x values
+        xFactor = float(self.get_field('##XFACTOR=')) # for our purposes it's 1, but if not use this instead
+        yFactor = float(self.get_field('##YFACTOR=')) # some very small number, but if not use this instead
+        self.xy = []
+        # Process the XY data from JCAMP's (X++(Y..Y)) format.
+        raw_xy = self.contents[self.contents.index('##XYDATA=(X++(Y..Y))') + 20:]
+        for match in re.finditer(r'(\D)([\d.-]+)', raw_xy):
+            if match.group(1) == '\n':
+                # Number is the first on the line and is an x-value
+                x = float(match.group(2)) * xFactor
+            else:
+                # Number is a relative y-value.
+                self.xy.append((x, float(match.group(2))*yFactor))
+                x += deltaX
+        # Keep the data in ascending order. It will be descending in the file
+        # if our delta X is negative.
+        if deltaX < 0:
+            self.xy.reverse()
+        # Integrate self.xy numerically over a fixed range.
+        range = (700.0, 3900.0)
+        # Initialize the data and find the interval of integration.
+        self.data = [0.0 for i in xrange(1000)]
+        interval = (range[1] - range[0]) / len(self.data)
+        # Find index in self.xy where integrals start
+        start = bisect.bisect_left(self.xy, (range[0], 0))
+        # oldX = start of range, oldY = linear interpolation of corresponding y
+        xy = self.xy
+        oldX, oldY = range[0], (self.xy[start - 1][1] +
+             (xy[start][1] - xy[start - 1][1]) * (range[0] - xy[start][0]) /
+             (xy[start - 1][0] - xy[start][0]))
+        for x, y in self.xy[start:]: #Iterate over self.xy from start
+            newIndex = int((x - range[0]) / interval)
+            oldIndex = int((oldX - range[0]) / interval)
+            if newIndex != oldIndex:
+                # We're starting a new integral.
+                boundary = newIndex * interval,\
+                           ((y - oldY) * (newIndex * interval - oldX) /
+                           (x - oldX) + oldY) #Linear interpolation
+                self.data[oldIndex] += (boundary[1] + oldY) * (boundary[0] - oldX) / 2 #Add area
+                if newIndex < len(self.data): # if data isn't filled 
+                    self.data[newIndex] += (boundary[1] + y) * (x - boundary[0]) / 2 #Add area
+            else:
+                self.data[newIndex] += (y + oldY) * (x - oldX) / 2 #Add area
+            if x > range[1]:
+                break #If finished, break
+            oldX, oldY = x, y #Otherwise keep going
+        self.chemical_type = 'Unknown' # We will find this later
+        # FIXME: Assumes chemical name is in TITLE label.
+        self.chemical_name = self.get_field('##TITLE=')
+        # Reference: http://www.jcamp-dx.org/
+    
+    ## Get a specific data label from the file.
+    # @param name Name of the data label to retrieve
+    # @return Value of the data label
+    def get_field(self, name):
+        """Get a specific data label from the file."""
+        index = self.contents.index(name) + len(name) # means find where the field name ends 
+        return self.contents[index:self.contents.index('\n', index)] #Does not handle Windows format
+    
+    def convert(self):
+        return Spectrum(chemical_name=self.chemical_name,
+                        chemical_type=self.chemical_type,
+                        data=self.data)
+        
 if __name__ == '__main__':
-	main()
+	main(os.getcwd())
