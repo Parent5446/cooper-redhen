0cef164a411f5c72643954ec50d69a317a8dbb8b
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-20 21:01:16 +0000

    html/main now reads filenames (in the compare handler)
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@213 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.py
+++ html/main.py
@@ -7,10 +7,13 @@ from pyjamas.ui.HTML import HTML
 from pyjamas.ui.Button import Button
 from pyjamas.ui.Grid import Grid
 from pyjamas.ui.AbsolutePanel import AbsolutePanel
+from pyjamas.ui.Image import Image
 
 if __name__ == '__main__':
-    panel = AbsolutePanel(StyleName='center-panel') #Contents can overlap
+    panel = AbsolutePanel(StyleName='center-panel') #So contents can overlap
     grid = Grid(2, 2)
+    file_upload = FileUpload(StyleName='file-upload')
+    uploaded_files = {}
 
     browse_list = ListBox(StyleName='listbox')
     map(browse_list.addItem, ['my computer', 'my projects', 'main library'])
@@ -19,17 +22,18 @@ if __name__ == '__main__':
         RootPanel().add(HTML('Browse '+browse_list.getSelectedItemText()[0]))
     
     compare_list = ListBox(StyleName='listbox')
-    map(compare_list.addItem, ['main library', 'my projects', 'each other'])
+    map(compare_list.addItem, ['to main library', 'to my projects', 'to each other'])
     def compare(event):
-        RootPanel().add(HTML('Compare '+compare_list.getSelectedItemText()[0]))
+        file = file_upload.getFilename()
+        if file not in uploaded_files: RootPanel().add(HTML('<i>'+file+'</i>'))
+        uploaded_files[file] = True
     
     grid.setWidget(0, 0, Button('<b>Browse</b>', browse, StyleName='button'))
     grid.setWidget(1, 0, Button('<b>Compare</b>', compare, StyleName='button'))
     grid.setWidget(0, 1, browse_list)
     grid.setWidget(1, 1, compare_list)
     panel.add(grid)
-    
-    file_upload = FileUpload(StyleName='file-upload')
     panel.add(file_upload, '-100px', '5px')
     
+    RootPanel().add(Image('banner.png'))
     RootPanel().add(panel)
