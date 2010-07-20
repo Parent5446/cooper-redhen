36d4ee38eb6ab165c9c1b317891588b07ffe942c
tylerromeo <tylerromeo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-20 03:16:23 +0000

    Put in invisible file upload box (called when you select "my computer" from drop down list. Moved directories around a little bit for easier builds.
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>
    
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@204 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.py
+++ html/main.py
@@ -1,10 +1,17 @@
 from pyjamas.ui.RootPanel import RootPanel
 from pyjamas.ui.VerticalPanel import VerticalPanel
 from pyjamas.ui.ListBox import ListBox
+from pyjamas.ui.FileUpload import FileUpload
 from pyjamas import Window
+from pyjamas import DOM
 
 def browse(event):
-    Window.alert(event.getSelectedItemText()[0])
+    if event.getSelectedItemText()[0] == "my computer":
+        # Get the invisible file uploader and click it.
+        file_upload = DOM.getElementById("file_upload")
+        DOM.buttonClick(file_upload)
+    else:
+        Window.alert(event.getSelectedItemText()[0])
     
 def compare(event):
     Window.alert(event.getSelectedItemText()[0])
@@ -12,14 +19,19 @@ def compare(event):
 if __name__ == '__main__':
     panel = VerticalPanel(StyleName='compare-panel')
  
-    browseList = ListBox()
-    for item in ['spectra', 'my computer', 'my projects', 'main library']: browseList.addItem('Browse '+item)
+    browse_list = ListBox()
+    map(browse_list.addItem, ['spectra', 'my computer', 'my projects', 'main library'])
     browseList.addChangeListener(browse)
-    compareList = ListBox()
-    for item in ['spectra', 'main library', 'my projects', 'each other']: compareList.addItem('Compare '+item)
+    
+    compare_list = ListBox()
+    map(compare_list.addItem, ['spectra', 'main library', 'my projects', 'each other'])
     compareList.addChangeListener(compare)
     
-    panel.add(browseList)
-    panel.add(compareList)
+    file_upload = FileUpload()
+    file_upload.setID("file_upload")
+    file_upload.setVisible(False)
     
+    panel.add(browse_list)
+    panel.add(compare_list)
+    panel.add(file_upload)
     RootPanel().add(panel)
