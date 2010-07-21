b57329422e94ba77c669c479a6fb0d3ba1fe073b
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-21 22:12:30 +0000

    Added suggestion list to browse dialog in html/BasicControls
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@222 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.py
+++ html/main.py
@@ -2,84 +2,16 @@
 A spectra-comparison website, which compiles to Javascript via Pyjamas (version 0.7).
 '''
 from pyjamas.ui.RootPanel import RootPanel
-from pyjamas.ui.ListBox import ListBox
-from pyjamas.ui.FileUpload import FileUpload
-from pyjamas import Window
-from pyjamas import DOM
-from pyjamas.ui.HTML import HTML
-from pyjamas.ui.Button import Button
-from pyjamas.ui.Grid import Grid
-from pyjamas.ui.AbsolutePanel import AbsolutePanel
 from pyjamas.ui.Image import Image
-from pyjamas.ui.VerticalPanel import VerticalPanel
-from pyjamas.ui.Composite import Composite
 from pyjamas.ui import HasAlignment
 from pyjamas.ui.DockPanel import DockPanel
-from pyjamas.ui.DialogBox import DialogBox
-from pyjamas.ui.TextBox import TextBox
 
-class BasicControls(Composite):
-    def __init__(self, StyleName=None):
-        Composite.__init__(self) #Superclass constructor
-        panel = AbsolutePanel(Size=('250px', None), StyleName=StyleName) #Panel which contains the controls
-        file_upload = FileUpload(StyleName='file-upload') #File upload control (not visible)
-        uploaded_files = {} #Table of uploaded files
-        
-        #Make list of 'browse' options:
-        browse_list = ListBox(StyleName='listbox')
-        map(browse_list.addItem, ['my computer', 'my projects', 'main library'])
-        browse_list.addChangeListener(lambda: file_upload.setVisible(browse_list.getSelectedItemText()[0]=='my computer'))
-        def browse(event):
-            dialog = DialogBox()
-            dock = DockPanel(StyleName='browse-dialog', Size=('250px', None))
-            dock.setSpacing(4)
-            dock.setHorizontalAlignment(HasAlignment.ALIGN_CENTER)
-            dock.add(HTML('Enter a chemical name from '+browse_list.getSelectedItemText()[0]+':'), DockPanel.NORTH)
-            input = TextBox()
-            def keyPressed(self, sender, keycode, modifiers):
-                text = input.getText()
-                if len(text) == 5:
-                    Window.alert('Looking up '+text)
-                elif len(text) > 5:
-                    Window.alert('Narrowing down '+text)
-            def done():
-                Window.alert( input.getText() )
-                dialog.hide()
-            dialog.onClick = done
-            listener = object()
-            listener.onKeyPress = lambda: None
-            listener.onKeyUp = keyPressed
-            listener.onKeyDown = lambda: None
-            input.addKeyboardListener(listener)
-            dock.add(input, DockPanel.CENTER)
-            dock.add(Button("Done", dialog), DockPanel.SOUTH) #Passes button clicks to dialog
-            dock.setWidth("100%")
-            dialog.setWidget(dock)
-            dialog.setPopupPosition(panel.getAbsoluteLeft(), panel.getAbsoluteTop()-10)
-            dialog.show()
-        
-        #Make list of 'compare' options:
-        compare_list = ListBox(StyleName='listbox')
-        map(compare_list.addItem, ['to main library', 'to my projects', 'to each other'])
-        def compare(event):
-            file = file_upload.getFilename()
-            if file not in uploaded_files: panel.add(HTML('<i>'+file+'</i>'))
-            uploaded_files[file] = True
-            
-        #Make grid layout:
-        grid = Grid(2, 2)
-        grid.setWidget(0, 0, Button('<b>Browse</b>', browse, StyleName='button'))
-        grid.setWidget(1, 0, Button('<b>Compare</b>', compare, StyleName='button'))
-        grid.setWidget(0, 1, browse_list)
-        grid.setWidget(1, 1, compare_list)
-        panel.add(grid)
-        panel.add(file_upload, '-100px', '5px') #This is a kludge (transparent file upload layered over a visible button)
-        self.initWidget(panel) #This panel becomes the center of the widget, and we're done
+from BasicControls import BasicControls
 
 if __name__ == '__main__':
-    dock = DockPanel(StyleName='centered-panel')
-    dock.setHorizontalAlignment(HasAlignment.ALIGN_CENTER)
-    dock.add(Image('banner.png'), DockPanel.NORTH)
-    dock.add(BasicControls(StyleName='frontpage-controls'), DockPanel.CENTER)
-    RootPanel().add(dock)
+    page = DockPanel(StyleName='centered-panel')
+    page.setHorizontalAlignment(HasAlignment.ALIGN_CENTER)
+    page.add(Image('banner.png'), DockPanel.NORTH)
+    page.add(BasicControls(StyleName='frontpage-controls'), DockPanel.CENTER)
+    RootPanel().add(page)
     
