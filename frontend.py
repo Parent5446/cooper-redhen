b5b490d40769416ac379ca79bc98789b57803295
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-21 17:30:14 +0000

    Simplified frontend.py ApiHandler to run tests with html/main
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@218 c84bbe80-f878-e576-0e26-96495b094a5b

--- frontend.py
+++ frontend.py
@@ -44,39 +44,11 @@ class ApiHandler(webapp.RequestHandler):
     """Handle any API requests and return a JSON response."""
     
     def get(self):
-        """
-        Handle a GET request to the API. Print instructions on how to use
-        the API.
-        """
-        self.response.out.write("""<pre>
-RedHen API v0.1
-
-All request must be POST requests. Any GET requests will just print these
-instructions, and all others will be ignored. The POST variables below
-may be passed. If uploading files, make sure to set enctype to
-multipart/form-data, or they will not be processed properly.
-
-- action (required):
-    - "compare" - Compare two targets, either from file upload or database.
-    - "analyze" - Does the same as compare, except instead of taking two
-                  targets, it takes one and searches the database in an
-                  attempt to find the other (good for unknown spectra).
-    - "browse"  - Browse the database.
-- target (required):
-    - When action is "search" or "compare": Can be either the text from a JCAMP
-      file or "db:key", where key is the database key for a Spectrum object.
-    - When action is "browse": Can be either "public" for browsing the public
-      library or "private" for browsing your own private library.
-- limit (optional, used only when action is "browse"): How many spectra to get
-  when browsing (maximum is 50).
-- offset (optional, used only when action is "browse"): Where to start listing
-  spectra from (used for pagination).
-- output (optional): Can be "xml", "json", "python", or "pickle" depending on
-                     what output format you want. Default is "pickle".
-        </pre>""")
+    '''Handle a GET request to the API.'''
+        self.response.out.write('Got request')
     
     def post(self):
-        """
+        '''
         Handle a POST request to the API.
         
         Take a list of actions from a POST request, and do the appropriate
@@ -84,7 +56,7 @@ multipart/form-data, or they will not be processed properly.
         
         @raise common.InputError: If no search targets are given in the target
         POST variable or if an invalid action is given.
-        """
+        '''
         action = self.request.get("action")
         targets = self.request.get_all("target")
         response = []
@@ -113,47 +85,17 @@ multipart/form-data, or they will not be processed properly.
         # Pass it on to self.output for processing.
         self.output(response)
     
-    def output(self, response):
-        """
-        Take a response from the script and process it for returning to the
-        user. Output formats include a serialized pickle object, JSON, XML,
-        or Python (simply running str() on the response.
-        
-        @param response: Server response to encode
-        @type  response: Mixed
-        @raise common.InputError: If an invalid output format is given.
-        """
-        format = self.request.get("output", "pickle")
-        if format == "pickle":
-            response = pickle.dumps(response)
-        elif format == "json":
-            from django.utils import simplejson
-            response = simplejson.dumps(response)
-        elif format == "xml":
-            response = self._convert_to_xml(response)
-        elif format == "python":
-            response = str(response)
-        else:
-            raise common.InputError(format, "Invalid output format.")
-        self.redirect("/serve?%s" % urllib.quote_plus(response))
-    
     def handle_exception(self, exception, debug_mode):
-        """
-        Handle any exceptions raised by the handler.
-        
-        This is called by App Engine if an exception is raised. If there is a
-        server error, let Google handle it appropriately. If there is an input
+        '''
+        This is called by App Engine if an exception is raised - if there is a
+        server error, let Google handle it appropriately; if there is an input
         error, print a custom error message.
-        
         @param exception: The Exception that was raised
         @type  exception: C{Exception} or one of its children
         @param debug_mode: Whether to print deubgging information or not
         @type  debug_mode: C{bool}
-        """
-        # TODO: Convert errors into a JSON response so the front end can handle
-        #       them easily.
-        if isinstance(exception, common.ServerError):
-            # Server error: notify user.
+        '''
+        if isinstance(exception, common.ServerError): # Server error: notify user.
             self.error(500)
             self.output(["ServerError", exception.msg])
         elif isinstance(exception, common.InputError):
@@ -161,61 +103,8 @@ multipart/form-data, or they will not be processed properly.
             # for all exception, but we want a 400 for an invalid request.
             self.error(400)
             self.output(["InputError", exception.expr, exception.msg])
-        else:
-            # Send all else to Google.
+        else: # Send all else to Google.
             super(ApiHandler, self).handle_exception(exception, True)
-
-    def _convert_to_xml(self, item):
-        """
-        Convert a given item to XML.
-        
-        @param item: Item to convert
-        @type  item: Mixed
-        @return: Response in XML format
-        @rtype: C{str}
-        """
-        xml = "<?xml version=\"1.0\"?>\n<response>"
-        xml += self._convert_to_xml_internal(item)
-        return xml + "</response>"
-    
-    def _convert_to_xml_internal(self, item):
-        """
-        Internal function for processing output into XML format.
-        
-        While the main function sets up the XML header, this actually converts
-        to XML. If item is a string, int, etc., just return it. If it is a
-        list, enclose each value in an <item></item> tag and run this function
-        recursively on whatever the value is. Do the same for dictionaries
-        except use the key as the tag name instead of <item>.
-        
-        @param item: Item to convert
-        @type  item: Mixed
-        @return: Response in XML format
-        @rtype: C{str}
-        """
-        if   (isinstance(item, str) or
-              isinstance(item, unicode) or
-              isinstance(item, int) or
-              isinstance(item, float)):
-            # Item is a string, unicode, integer, or float. Just return it.
-            return str(item)
-        elif (isinstance(item, list) or
-              isinstance(item, tuple)):
-            # Item is a list or tuple. Enclose each item in <item></item> and
-            # run this function recursively on each item.
-            xml = ""
-            for value in item:
-                xml += "<item>" + self._convert_to_xml_internal(value) + "</item>"
-            return xml
-        elif isinstance(item, dict):
-            # Item is a dictionary. Do the same as is done above for a list,
-            # except use the keys as tag names.
-            for key, value in item.iteritems():
-                xml += "<" + key + ">" + self._convert_to_xml_internal(item) + "</" + key + ">"
-            return xml
-        else:
-            # For everything else, use pickle to dump it.
-            return pickle.dumps(item)
 
 
 class ServeHandler(webapp.RequestHandler):
