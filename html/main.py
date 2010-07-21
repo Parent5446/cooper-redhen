60a7a14888184719c14d92a35758240bf7158a52
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-21 15:14:02 +0000

    Created StartPage class in html/main.py
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@216 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.py
+++ html/main.py
@@ -8,32 +8,49 @@ from pyjamas.ui.Button import Button
 from pyjamas.ui.Grid import Grid
 from pyjamas.ui.AbsolutePanel import AbsolutePanel
 from pyjamas.ui.Image import Image
+from pyjamas.ui.VerticalPanel import VerticalPanel
+from pyjamas.ui.Composite import Composite
+from pyjamas.ui import HasAlignment
+from pyjamas.ui.DockPanel import DockPanel
+
+class StartPage(Composite):
+    def __init__(self):
+        Composite.__init__(self)
+        panel = AbsolutePanel(StyleName='start-control-panel')
+        grid = Grid(2, 2)
+        file_upload = FileUpload(StyleName='file-upload')
+        uploaded_files = {}
+
+        browse_list = ListBox(StyleName='listbox')
+        map(browse_list.addItem, ['my computer', 'my projects', 'main library'])
+        browse_list.addChangeListener(lambda: file_upload.setVisible(browse_list.getSelectedItemText()[0]=='my computer'))
+        def browse(event):
+            if browse_list.getSelectedItemText()[0] == 'my projects': pass
+            elif browse_list.getSelectedItemText()[0] == 'main library': pass
+        
+        compare_list = ListBox(StyleName='listbox')
+        map(compare_list.addItem, ['to main library', 'to my projects', 'to each other'])
+        def compare(event):
+            file = file_upload.getFilename()
+            if file not in uploaded_files: self.add(HTML('<i>'+file+'</i>'))
+            uploaded_files[file] = True
+        
+        grid.setWidget(0, 0, Button('<b>Browse</b>', browse, StyleName='button'))
+        grid.setWidget(1, 0, Button('<b>Compare</b>', compare, StyleName='button'))
+        grid.setWidget(0, 1, browse_list)
+        grid.setWidget(1, 1, compare_list)
+        panel.add(grid)
+        panel.add(file_upload, '-100px', '5px')
+        
+        vertical = VerticalPanel()
+        vertical.add(Image('banner.png'))
+        vertical.add(panel)
+        vertical.setHorizontalAlignment(HasAlignment.ALIGN_CENTER)
+        vertical.setWidth("100%")
+        self.initWidget(vertical)
 
 if __name__ == '__main__':
-    panel = AbsolutePanel(StyleName='center-panel') #So contents can overlap
-    grid = Grid(2, 2)
-    file_upload = FileUpload(StyleName='file-upload')
-    uploaded_files = {}
-
-    browse_list = ListBox(StyleName='listbox')
-    map(browse_list.addItem, ['my computer', 'my projects', 'main library'])
-    browse_list.addChangeListener(lambda: file_upload.setVisible(browse_list.getSelectedItemText()[0]=='my computer'))
-    def browse(event):
-        RootPanel().add(HTML('Browse '+browse_list.getSelectedItemText()[0]))
+    dp = DockPanel(StyleName='start-panel')
+    dp.add(StartPage(), DockPanel.CENTER)
+    RootPanel().add(dp)
     
-    compare_list = ListBox(StyleName='listbox')
-    map(compare_list.addItem, ['to main library', 'to my projects', 'to each other'])
-    def compare(event):
-        file = file_upload.getFilename()
-        if file not in uploaded_files: RootPanel().add(HTML('<i>'+file+'</i>'))
-        uploaded_files[file] = True
-    
-    grid.setWidget(0, 0, Button('<b>Browse</b>', browse, StyleName='button'))
-    grid.setWidget(1, 0, Button('<b>Compare</b>', compare, StyleName='button'))
-    grid.setWidget(0, 1, browse_list)
-    grid.setWidget(1, 1, compare_list)
-    panel.add(grid)
-    panel.add(file_upload, '-100px', '5px')
-    
-    RootPanel().add(Image('banner.png'))
-    RootPanel().add(panel)
