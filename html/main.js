2df705e53b25aebd3ea64eb3eee5014d6ab80970
jminuse <jminuse@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-07-31 16:00:19 +0000

    Moved jquery plugin to the script end, removed 'use strict' (current browsers don't enforce it, and we don't know how it will affect the code if they ever do).
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@342 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.js
+++ html/main.js
@@ -1,60 +1,5 @@
 ﻿/*global jQuery */
 /*jslint white: true, browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, bitwise: true, regexp: true, newcap: true, strict: true */
-/**
- * jQuery plugin for posting form including file inputs.
- * 
- * Copyright (c) 2010 Ewen Elder
- *
- * Licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- * @author: Ewen Elder <glomainn at yahoo dot co dot uk> <ewen at jainaewen dot com>
- * @version: 1.0.1 (2010-07-22)
-**/
-
-'use strict';
-(function ($) {
-    $.fn.iframePostForm = function (options) {
-        var contents, elements, element, iframe;
-        
-        elements = $(this);
-        options = $.extend({}, $.fn.iframePostForm.defaults, options);
-        
-        // Add the iframe.
-        if (!$('#' + options.iframeID).length) {
-            $('body').append('<iframe name="' + options.iframeID + '" id="' + options.iframeID + '" style="display:none"></iframe>');
-        }
-
-        return elements.each(function () {
-            element = $(this);
-            // Target the iframe.
-            element.attr('target', options.iframeID);
-            // Submit listener.
-            element.submit(function() {
-                options.post.apply(this);
-                iframe = $('#' + options.iframeID);
-                iframe.one('load', function() {
-                    options.complete.apply(this, [$("#" + this.id).contents().text()]);
-                    setTimeout(function() {
-                        $("#" + this.id).contents().text('');
-                    }, 1);
-                });
-            });
-        });
-    };
-    
-    $.fn.iframePostForm.defaults = {
-        iframeID : 'iframe-post-form',       // IFrame ID.
-        post : function () {},               // Form onsubmit.
-        complete : function (response) {}    // After everything is completed.
-    };
-})(jQuery);
-
-/**
- * End Jquery Plugin.
- * Begin RedHen LGPL.
- */
 
 function got_results(response) { //Once the server has responded
     //TIME_TEST = (new Date()).getTime();
@@ -263,3 +208,50 @@ $(document).ready(function() {
     $("#upload_form").append("<input type='file' class='invisible-frame' id='file1' name='spectrum' />")
     $("#file1").change(onchange_file);
 });
+
+/**
+ * jQuery plugin for posting form including file inputs.
+ * Copyright (c) 2010 Ewen Elder
+**/
+
+(function ($) {
+    $.fn.iframePostForm = function (options) {
+        var contents, elements, element, iframe;
+        
+        elements = $(this);
+        options = $.extend({}, $.fn.iframePostForm.defaults, options);
+        
+        // Add the iframe.
+        if (!$('#' + options.iframeID).length) {
+            $('body').append('<iframe name="' + options.iframeID + '" id="' + options.iframeID + '" style="display:none"></iframe>');
+        }
+
+        return elements.each(function () {
+            element = $(this);
+            // Target the iframe.
+            element.attr('target', options.iframeID);
+            // Submit listener.
+            element.submit(function() {
+                options.post.apply(this);
+                iframe = $('#' + options.iframeID);
+                iframe.one('load', function() {
+                    options.complete.apply(this, [$("#" + this.id).contents().text()]);
+                    setTimeout(function() {
+                        $("#" + this.id).contents().text('');
+                    }, 1);
+                });
+            });
+        });
+    };
+    
+    $.fn.iframePostForm.defaults = {
+        iframeID : 'iframe-post-form',       // IFrame ID.
+        post : function () {},               // Form onsubmit.
+        complete : function (response) {}    // After everything is completed.
+    };
+})(jQuery);
+
+/**
+ * End Jquery Plugin.
+ * Begin RedHen LGPL.
+ */
