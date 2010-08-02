dbe17c6afc7131d37643957346e3280054bd2460
tylerromeo <tylerromeo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-08-02 03:08:24 +0000

    Removing peak table. There is no longer a need for it since the find_similar function uses peaks just to sort the heavyside-selected keys, which is useless since they will just be resorted when Bove/Leastsquares is run.
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>
    
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@356 c84bbe80-f878-e576-0e26-96495b094a5b

--- backend.py
+++ backend.py
@@ -584,9 +584,6 @@ def find_similar(spectrum):
     key = spectrum.spectrum_type+'_heavyside_'+str(spectrum.calculate_heavyside()) #Get the heavyside key
     spectra = get_data(key, default=None) #Get the spectra that match the key
     
-    keys = sorted(spectra, key=lambda s: min([s[0]-peak for peak in peaks]) ) #Sort by smallest distance to peaks
-    if len(keys) > 10: keys = keys[:10] 
-    
     return Spectrum.get([k[1] for k in keys])
     
 def bove(a, b):
