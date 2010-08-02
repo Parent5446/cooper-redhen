4f484fe52df979083b0c952cf54da9f515017359
tylerromeo <tylerromeo@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-08-02 01:18:50 +0000

    Putting in test.py, a nice script that simulates 2000 users in 8 threads (each with one search suggestion request and one compare request). Results look good.
    Signed-off-by: Tyler Romeo <tylerromeo@gmail.com>
    
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@354 c84bbe80-f878-e576-0e26-96495b094a5b

---
+++ test.py
@@ -0,0 +1,63 @@
+import httplib, urllib, mimetypes, threading
+
+class Thread(threading.Thread):
+    def __init__(self, num, *args, **kwargs):
+        self.num = num
+        super(Thread, self).__init__(*args, **kwargs)
+        
+    def run(self):
+        num = self.num
+        sugg_data = [("action", "browse"), ("type", "infrared"),
+                     ("guess", "iodo"), ("output", "json")]
+        sugg_headers = {"Content-type": "application/x-www-form-urlencoded",
+                        "Content-Length": 50,
+                        "Accept": "text/plain"}
+        search_data = [("action", "compare"), ("targt", "public"), ("output", "json")]
+        search_files = [("spectrum", "jcamp-test.jdx", open("jcamp-test.jdx").read())]
+        conn = httplib.HTTPConnection("cooper-redhen.appspot.com")
+        body1, headers1 = self.post_multipart(sugg_data)
+        body2, headers2 = self.post_multipart(search_data, search_files)
+
+        for key in xrange(250):
+            conn.request("POST", "/api", body1, headers1)
+            res = conn.getresponse()
+            print "SEARCH  ", num, " ", key, ":", res.status, res.reason
+            if res.status != 200:
+                raise Exception()
+            conn.request("POST", "/api", body2, headers2)
+            res = conn.getresponse()
+            print "COMPARE ", num, " ", key, ":", res.status, res.reason
+            if res.status != 200:
+                raise Exception()
+    
+    def post_multipart(self, fields, files=[]):
+        """
+        Post fields and files to an http host as multipart/form-data.
+        fields is a sequence of (name, value) elements for regular form fields.
+        files is a sequence of (name, filename, value) elements for data to be uploaded as files
+        Return the server's response page.
+        """
+        BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+        CRLF = '\r\n'
+        L = []
+        for (key, value) in fields:
+            L.append('--' + BOUNDARY)
+            L.append('Content-Disposition: form-data; name="%s"' % key)
+            L.append('')
+            L.append(value)
+        for (key, filename, value) in files:
+            L.append('--' + BOUNDARY)
+            L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+            L.append('Content-Type: %s' % (mimetypes.guess_type(filename)[0] or 'application/octet-stream'(filename)))
+            L.append('')
+            L.append(value)
+        L.append('--' + BOUNDARY + '--')
+        L.append('')
+        body = CRLF.join(L)
+
+        headers = {"Content-type": 'multipart/form-data; boundary=%s' % BOUNDARY,
+                          "Content-length": str(len(body))}
+        return body, headers
+
+for num in xrange(8):
+    Thread(num).start()
