40e20bf28325773d9c8c50d6e91a956e2d0bb1d4
mottaqui.karim.9 <mottaqui.karim.9@c84bbe80-f878-e576-0e26-96495b094a5b>
2010-08-03 18:02:52 +0000

    hopefully made browse dialog large enough to conmpletely cover front controls
    git-svn-id: https://cooper-redhen.googlecode.com/svn/trunk@394 c84bbe80-f878-e576-0e26-96495b094a5b

--- html/main.js
+++ html/main.js
@@ -462,7 +462,7 @@ $(document).ready(function() {
     });
     $("#upload_form").append("<input type='file' class='invisible-frame' id='file1' name='spectrum' />")
     $("#file1").change(onchange_file);
-    if (('#browse_options').options[0].selected != 'selected') {
-            ('#browse_options').options[0].selected = 'selected';
+    if (('#browse_options option:selected') != 'selected') {
+            ('#browse_options option:selected') = 'selected';
     }
 });
